659320f85c0cb7c0462bc4e6e8d0eeb16e74cb28
Đỗ Cao Minh <caominhbaby010@gmail.com>
2022-03-03 13:15:06 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+//error xu ly loi
+const { BadRequestError, errorHandler } = require("./app/errors");
 
 const app = express();
 
@@ -10,7 +12,21 @@ app.get("/", (req, res) => {
 });
 
 // set app
-const setupContactRoutes = require('./app/routes/contact.routes');
+const setupContactRoutes = require("./app/routes/contact.routes");
 setupContactRoutes(app);
+
+// handle 404 response
+app.use((req, res, next) => {
+  // Code ở đây sẽ chạy khi không có route được định nghĩa nào
+  // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+  next(new BadRequestError(404, "Resource not found"));
+});
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+  // Middleware xử lý lỗi tập trung.
+  // Trong các đoạn code xử lý ở các route, gọi next(error)
+  // sẽ chuyển về middleware xử lý lỗi này
+  errorHandler.handleError(error, res);
+});
 
 module.exports = app;
--- app/routes/contact.routes.js
+++ app/routes/contact.routes.js
@@ -4,26 +4,35 @@ const ContactController = require("../controllers/contact.controller");
 module.exports = (app) => {
   const router = express.Router();
   const contacts = new ContactController();
-  //create a new contact
-  router.post("/", contacts.create);
+  router.route("/")
+    .post(contacts.create)
+    .get(contacts.findAll)
+    .delete(contacts.deleteAll);
+  router.route("/:id")
+    .get(contacts.findOne)
+    .put(contacts.update)
+    .delete(contacts.deleteOne)
+  router.route("/favorite")
+    .get(contacts.findAllFavorite);
+//   //create a new contact
+//   router.post("/", contacts.create);
+//   //retrieve all contacts
+//   router.get("/", contacts.findAll);
 
-  //retrieve all contacts
-  router.get("/", contacts.findAll);
+//   // //retrieve all favorite contacts
+//   router.get("/favorite", contacts.findAllFavorite);
 
-  // //retrieve all favorite contacts
-  router.get("/favorite", contacts.findAllFavorite);
+//   // // //retrieve a single contact with id
+//   router.get("/:id", contacts.findOne);
 
-  // // //retrieve a single contact with id
-  router.get("/:id", contacts.findOne);
+//   // // //update a contact with id
+//   router.put("/:id", contacts.update);
 
-  // // //update a contact with id
-  router.put("/:id", contacts.update);
+//   // // //delete a contact with id
+//   router.delete("/:id", contacts.deleteOne);
 
-  // // //delete a contact with id
-  router.delete("/:id", contacts.deleteOne);
-
-  // // //delete all contacts
-  router.delete("/", contacts.deleteAll);
+//   // // //delete all contacts
+//   router.delete("/", contacts.deleteAll);
 
   app.use("/api/contacts", router);
 };
