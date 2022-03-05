344a4f3afdc51c983704732bd1bd41be1fb6d9b6
Đỗ Cao Minh <caominhbaby010@gmail.com>
2022-03-05 19:01:16 +0700

    Cai dat cac handler truy xuat CSDL

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,39 +1,182 @@
-module.exports = class ContactController {
-  //create and save a new contact
-  create = async (req, res) => {
-    res.send({ message: "create handle" });
-  };
+const { BadRequestError } = require("../errors");
+const handlePromise = require("../helpers/promise.helper");
+const mongoose = require("mongoose");
+const Contact = require("../models/contact.model");
 
-  //Retrieve all contacts of a user from the database.
-  findAll = async (req, res) => {
-    res.send({ message: "findAll handle" });
-  };
+// module.exports = class ContactController {
+//   //create and save a new contact
+//   create = async (req, res) => {
+//     res.send({ message: "create handler" });
+//   };
 
-  //Retrieve all favorite contacts
-  findAllFavorite = async (req, res) => {
-    res.send({ message: "findAllFavorite handle" });
-  };
+//Create and save a new contact
+exports.create = async (req, res, next) => {
+  //validate request
+  if (!req.body.name) {
+    return next(new BadRequestError(400, "Name can not be empty"));
+  }
 
-  //Find a single contact with a id
-  findOne = async (req, res) => {
-    res.send({ message: "findOne handle" });
-  };
+  //Create a contact
+  const contact = new Contact({
+    name: req.body.name,
+    email: req.body.email,
+    address: req.body.address,
+    phone: req.body.phone,
+    favorite: req.body.favorite === true,
+  });
 
-  //Update a contact by a id in the request
-  update = async (req, res) => {
-    res.send({ message: "update handle" });
-  };
+  //save contact in the database
+  const [error, document] = await handlePromise(contact.save());
 
-  // Delete a contact with the specified id in the request
-  deleteOne = async (req, res) => {
-    res.send({ message: "delete handle" });
-  };
-  // Delete all contacts of a user from the database
-  deleteAll = async (req, res) => {
-    res.send({ message: "deleteAll handle" });
-  };
+  if (error) {
+    return next(
+      new BadRequestError(500, "An error occurred while creating the contact")
+    );
+  }
+
+  return res.send(document);
 };
-//module.exports = {create, findAll, findAllFavorite, findOne, update, deleteAll, deleteOne}
+
+//Retrieve all contacts of a user from the database.
+exports.findAll = async (req, res, next) => {
+  const condition = {};
+  const { name } = req.query;
+  if (name) {
+    condition.name = { $regex: new RegExp(name), $options: "i" };
+  }
+
+  const [error, documents] = await handlePromise(Contact.find(condition));
+
+  if (error) {
+    return next(
+      new BadRequestError(500, "An error occurred while retrieving contacts")
+    );
+  }
+
+  return res.send(documents);
+};
+
+//Find a single contact with a id
+exports.findOne = async (req, res, next) => {
+  const { id } = req.params;
+  const condition = {
+    _id: id && mongoose.isValidObjectId(id) ? id : null,
+  };
+
+  try {
+    const document = await Contact.findOne(condition);
+    if (!document) {
+      return next(new BadRequestError(404, "Contact not found"));
+    }
+
+    return res.send(document);
+  } catch (e) {
+    return next(
+      new BadRequestError(
+        500,
+        `Error retrieving contact with id=$(req.params.id})`
+      )
+    );
+  }
+
+  // const [error, document] = await handlePromise(Contact.findOne(condition));
+
+  // if(error){
+  //   return next(new BadRequestError(500, `Error retrieving contact with id=$(req.params.id})`));
+  // }
+
+  // if(!document){
+  //   return next(new BadRequestError(404, "Contact not found"));
+  // }
+
+  // return res.send(document);
+};
+
+//Update a contact by a id in the request
+exports.update = async (req, res, next) => {
+  if (Object.keys(req.body).length === 0) {
+    return next(new BadRequestError(400, "Data to update can not be empty"));
+  }
+
+  const { id } = req.params;
+  const condition = {
+    _id: id && mongoose.isValidObjectId(id) ? id : null,
+  };
+
+  const [error, document] = await handlePromise(
+    Contact.findOneAndUpdate(condition, req.body, { new: true })
+  );
+
+  if (error) {
+    return next(
+      new BadRequestError(
+        500,
+        `Error updating contact with id=$(req.params.id}`
+      )
+    );
+  }
+
+  if (!document) {
+    return next(new BadRequestError(404, "Contact not found"));
+  }
+
+  return res.send({ message: "Contact was updated successfully" });
+};
+
+// Delete a contact with the specified id in the request
+exports.deleteOne = async (req, res) => {
+  const { id } = req.params;
+  const condition = {
+    _id: id && mongoose.isValidObjectId(id) ? id : null,
+  };
+
+  const [error, document] = await handlePromise(
+    Contact.findOneAndDelete(condition)
+  );
+
+  if (error) {
+    return next(
+      new BadRequestError(
+        500,
+        `Could not delete contact with id=$(req.params.id}`
+      )
+    );
+  }
+
+  if (!document) {
+    return next(new BadRequestError(404, "Contact not found"));
+  }
+
+  res.send({ message: "Contact was deleted successfully" });
+};
+
+//Retrieve all favorite contacts
+exports.findAllFavorite = async (req, res) => {
+  const [error, documents] = await handlePromise(
+    Contact.find({favorite: true,})
+  );
+
+  if(error){
+    return next(new BadRequestError(500,"An error occurred while retrieving favorite contacts"));
+  }
+
+  return res.send(documents);
+};
+
+// Delete all contacts of a user from the database
+exports.deleteAll = async (req, res) => {
+  const [error, data] = await handlePromise(Contact.deleteMany({}));
+
+  if (error) {
+    return next(
+      new BadRequestError(500, "An error occurred while removing all contacts")
+    );
+  }
+
+  return res.send({
+    message: `${data.deleteCount} contacts were deleted successfully`,
+  });
+};
 
 //cach 2 dung class
 // module.exports = class ContactController{
--- app/routes/contact.routes.js
+++ app/routes/contact.routes.js
@@ -1,38 +1,38 @@
 const express = require("express");
-const ContactController = require("../controllers/contact.controller");
+const contacts = require("../controllers/contact.controller");
 
 module.exports = (app) => {
   const router = express.Router();
-  const contacts = new ContactController();
-  router.route("/")
-    .post(contacts.create)
-    .get(contacts.findAll)
-    .delete(contacts.deleteAll);
-  router.route("/:id")
-    .get(contacts.findOne)
-    .put(contacts.update)
-    .delete(contacts.deleteOne)
-  router.route("/favorite")
-    .get(contacts.findAllFavorite);
-//   //create a new contact
-//   router.post("/", contacts.create);
-//   //retrieve all contacts
-//   router.get("/", contacts.findAll);
+  //create a new contact
+  router.post("/", contacts.create);
+  //retrieve all contacts
+  router.get("/", contacts.findAll);
+  
+  // //retrieve all favorite contacts
+  router.get("/favorite", contacts.findAllFavorite);
+  
+  // // //retrieve a single contact with id
+  router.get("/:id", contacts.findOne);
+  
+  // // //update a contact with id
+  router.put("/:id", contacts.update);
+  
+  // // //delete a contact with id
+  router.delete("/:id", contacts.deleteOne);
+  
+  // // //delete all contacts
+  router.delete("/", contacts.deleteAll);
+  // router.route("/")
+  //   .post(contacts.create)
+  //   .get(contacts.findAll)
+  //   .delete(contacts.deleteAll);
+  // router.route("/:id")
+  //   .get(contacts.findOne)
+  //   .put(contacts.update)
+  //   .delete(contacts.deleteOne)
+  // router.route("/favorite")
+  //   .get(contacts.findAllFavorite);
 
-//   // //retrieve all favorite contacts
-//   router.get("/favorite", contacts.findAllFavorite);
+ app.use("/api/contacts", router);
 
-//   // // //retrieve a single contact with id
-//   router.get("/:id", contacts.findOne);
-
-//   // // //update a contact with id
-//   router.put("/:id", contacts.update);
-
-//   // // //delete a contact with id
-//   router.delete("/:id", contacts.deleteOne);
-
-//   // // //delete all contacts
-//   router.delete("/", contacts.deleteAll);
-
-  app.use("/api/contacts", router);
 };
